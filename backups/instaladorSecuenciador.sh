e685707afcfa386641cb3bc997bbbded73c18dad
Eduardo Serrano <axenedu@gmail.com>
2020-11-17 18:37:41 +0100

    ajustes finales

--- backups/instaladorSecuenciador.sh
+++ backups/instaladorSecuenciador.sh
@@ -2,8 +2,8 @@
   
 #Para instalar copia y pega el siguiente código 
 #También puedes descargar el archivo y ejecutarlo
-#Ejecutar en una máquina vagrant limpia
-curl -o secuenciador.sh https://raw.githubusercontent.com/axenedu/seguridad-informatica/main/backups/secuenciador.sh
-curl -o crontab https://raw.githubusercontent.com/axenedu/seguridad-informatica/main/backups/crontab 
+#Ejecutar en el directorio $HOME de una máquina vagrant limpia
+curl -o $HOME/secuenciador.sh https://raw.githubusercontent.com/axenedu/seguridad-informatica/main/backups/secuenciador.sh
+curl -o $HOME/crontab https://raw.githubusercontent.com/axenedu/seguridad-informatica/main/backups/crontab 
 crontab ./crontab
 
