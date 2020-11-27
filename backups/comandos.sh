c5569afcb87fc8aefa593dd37f93a0065852da82
Eduardo Serrano <axenedu@gmail.com>
2020-11-27 09:42:39 +0100

    modifica link comandos.sh

--- backups/comandos.sh
+++ backups/comandos.sh
@@ -1,5 +1,5 @@
 #crear directorios de trabajo
-#curl -o comandos.sh https://raw.githubusercontent.com/axenedu/scripts/main/examen_backups/plantilla/comandos.sh?token=AAIJOOQUNDXU2XPKEXTYXGS7XWGDW 
+#curl -o comandos.sh https://raw.githubusercontent.com/axenedu/seguridad-informatica/main/backups/comandos.sh 
 #sh comandos.sh
 
 #crea directorios base
@@ -8,7 +8,7 @@ mkdir $HOME/examen/copias
 mkdir $HOME/examen/comandos
 
 #Declaración de las url como constantes
-SECUENCIADOR="https://raw.githubusercontent.com/axenedu/scripts/main/examen_backups/solucion/secuenciador.sh?token=AAIJOOUL2EL2ZYA5ZJHC7TS7XWD5A"
+SECUENCIADOR="https://raw.githubusercontent.com/axenedu/seguridad-informatica/main/backups/secuenciador.sh"
 
 
 #Descarga herramienta para crear contenido automáticamente
